067d5bc9f62ba415f3238eab83ff88bddf0dd29f
Caue de Freitas Adriano <caue.adriano@outlook.com>
2024-06-10 12:45:35 -0300

    Começo da API

--- js/main.js
+++ js/main.js
@@ -1,39 +1,258 @@
 console.log("teste de conexão")
 
-function addCard() {
-    const main = document.querySelector('.card-position')
-    main.innerHTML = main.innerHTML + `
-    <div class="card-ticker">
+// adicione seu token
+const token = '___aqui___'
+
+const allStocks = [
+	{ 
+		bolsa: "NASDAQ",
+		codigo: "AAPL",
+		empresa: "Apple Inc",
+		valor: 25080,
+		variacao: 0.35,
+		nAcoes: 40
+	},
+	{
+		bolsa: "NASDAQ",
+		codigo: "MSFT",
+		empresa: "Microsoft Corp",
+		valor: 50234,
+		variacao: -1.5,
+		nAcoes: 20
+	},
+	{
+		bolsa: "NASDAQ",
+		codigo: "META",
+		empresa: "Meta Platforms Inc",
+		valor: 43262,
+		variacao: 2.3,
+		nAcoes: 5
+	},
+	{
+		bolsa: "NASDAQ",
+		codigo: "GOOGL",
+		empresa: "Alphabet Class A",
+		valor: 16615,
+		variacao: -0.78,
+		nAcoes: 8
+	},
+	{
+		bolsa: "NASDAQ",
+		codigo: "NVDA",
+		empresa: "NVIDIA Corp",
+		valor: 87757,
+		variacao: 0.02,
+		nAcoes: 13
+	}
+]
+
+function addCard({bolsa, codigo, empresa, valor, variacao, nAcoes}){
+    //const main = document.querySelector('body > main')
+    
+		main.innerHTML += `
+        <div class="card-ticker" id="${codigo}" onmouseenter="cardEnter(event)" onmouseleave="cardLeave(event)">
+			<header>
+				<h2><span>${bolsa}:</span> ${codigo}</h2>
+				<h1>${empresa}</h1>
+			</header>
+			<main>
+				<p>${realFormat(+valor / 100)}</p>
+				<span ${ variacao < 0 ? 'style="background: #FF0000;"' : ''} >${ variacao < 0 ? '▼' : '▲'} ${variacao}%</span>
+				<span>${realFormat(((+valor / 100)*(variacao / 100)))}</span>
+			</main>
+			<footer>
+				<div>
+					<p>${nAcoes}</p>
+					<span>Ações</span>
+				</div>
+				<div>
+					<p>R$ ${realFormat(nAcoes * (+valor / 100))}</p>
+					<span>Posição</span>
+				</div>
+			</footer>
+			<div class="card-menu">
+				<span>Editar</span>
+				<span onclick="removeCard(event)">Excluir</span>
+			</div>
+		</div>
+	`
+	}
+
+	function updateCard({bolsa, codigo, empresa, valor, variacao, nAcoes}){
+		//const {bolsa, codigo, empresa, valor, variacao, nAcoes} = stock
+	
+		const card = document.querySelector(`#${codigo}`)
+	
+		card.innerHTML = `
 				<header>
-					<h2><span>NASDAQ:</span> AAPL</h2>
-					<h1>Apple Inc</h1>
+					<h2><span>${bolsa}:</span> ${codigo}</h2>
+					<h1>${empresa}</h1>
 				</header>
 				<main>
-					<p>R$ 250,80</p>
-					<span>▲ 0,35%</span>
-					<span>R$ 0,60</span>
+					<p>${realFormat(+valor / 100)}</p>
+					<span ${ variacao < 0 ? 'style="background: #FF0000;"' : ''} >${ variacao < 0 ? '▼' : '▲'} ${variacao}%</span>
+					<span>${realFormat(((+valor / 100)*(variacao / 100)))}</span>
 				</main>
 				<footer>
 					<div>
-						<p>40</p>
+						<p>${nAcoes}</p>
 						<span>Ações</span>
 					</div>
 					<div>
-						<p>R$ 34853,45</p>
+						<p>${realFormat(nAcoes * (+valor / 100))}</p>
 						<span>Posição</span>
 					</div>
 				</footer>
-			</div>
-            `
+				<div class="card-menu">
+					<span>Editar</span>
+					<span onclick="removeCard(event)">Excluir</span>
+				</div>
+		`
+	}
+
+	function realFormat(valor){
+		//return valor.toFixed(2).toString().replace('.',',')
+		return new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(valor)
+	}
+	
+	function loadCards(){
+		allStocks.map(stock => addCard(stock))
+	}
+
+	function loadTable(){
+		const table = document.querySelector('#table-acoes')
+		let rows = table.innerHTML
+		allStocks.map(stock => {
+			rows += 
+				`<tr>
+					<td>${stock.bolsa}</td>
+					<td>${stock.codigo}</td>
+					<td>${stock.empresa}</td>
+					<td>${realFormat(stock.valor / 100)}</td>
+					<td>${stock.variacao} %</td>
+					<td>${stock.nAcoes}</td>
+					<td>${realFormat((stock.valor / 100) * stock.nAcoes)}</td>
+				</tr>`
+		})
+		table.innerHTML = rows
+	}
+
+	const openModal = (idModal) => {
+		const modal = document.getElementById(idModal)
+		modal.style.display = 'flex'
+	}
+	
+	const closeModal = (event, id) => {
+		if(id){
+			const modal = document.getElementById(id)
+			modal.style.display = 'none'
+			return
+		}
+	
+		if(event?.target?.className === "modal"){
+			const modal = document.getElementById(event.target.id)
+			modal.style.display = 'none'
+			return
+		}
+	}
+
+	const createCard = (event) =>{
+		event.preventDefault()
+	
+		// const {bolsa, codigo, empresa, valor, variacao, nAcoes} = event.target.elements
+		// const stock = {
+		// 	bolsa: bolsa.value,
+		// 	codigo: codigo.value,
+		// 	empresa: empresa.value,
+		// 	valor: valor.value,
+		// 	variacao: variacao.value,
+		// 	nAcoes: nAcoes.value
+		// }
+	
+		const formData = new FormData(event.target)
+		const stock = Object.fromEntries(formData)
+		console.log(stock)
+	
+		addCard(stock)
+		event.target.reset()
+		closeModal(null, 'add-form-modal')
 }
 
-//dados do ticker
- const teste = {
-    nasdaq: "teste",
-    nome: "teste1",
-    valor: 100.50,
-    porcentagem: 5.6,
-    valorextra: 1.00,
-    acoes: 55,
-    posicao: 99999.99
- }
+// async function testeApi(){
+// }
+const testeApi = async () => {
+	const response = await fetch('https://finnhub.io/api/v1/quote?symbol=AAPL&token=cp1aj71r01qu1k1i369gcp1aj71r01qu1k1i36a0')
+	//const response = await fetch('https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&token=cp1aj71r01qu1k1i369gcp1aj71r01qu1k1i36a0')
+
+	console.log(response)
+	const result = await response.json()
+	console.log(result.objectName)
+}
+
+const createApiCard = async (event) =>{
+	event.preventDefault()
+	const {codigo, nAcoes} = event.target.elements
+
+	try {
+		const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${codigo.value}&token=${token}`)
+		const result = await response.json()
+		console.log("Result:", result)
+
+		const response2 = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${codigo.value}&token=${token}`)
+		const profile = await response2.json()
+		console.log("Profile:", profile)
+
+
+		if(!response.ok || !response2.ok){
+			alert('Erro ao consultar ação!')
+			return
+		}
+
+		if(profile?.exchange === undefined || result?.d === null){
+			alert('Ação não encontrada!')
+			return
+		}
+
+		const stock = {
+			bolsa: profile.exchange.split(' ')[0],
+			codigo: codigo.value,
+			empresa: profile?.name || '',
+			valor: result.c * 100,
+			variacao: result.d,
+			nAcoes: nAcoes.value
+		}
+
+		const card = document.getElementById(codigo.value)
+
+		if(card){
+			updateCard(stock)
+		} else {
+			addCard(stock)
+		}
+	} catch (error){
+		alert('Erro ao consultar ação!')
+		console.log('ERROR:', error)
+	}
+
+	event.target.reset()
+	closeModal(null, 'add-api-modal')
+}
+
+const cardEnter = (event) => {
+	const cardMenu = event.target.querySelector('.card-menu')
+	cardMenu.style.display = 'flex'
+	console.log(event.target)
+	console.log('cardEnter....')
+}
+
+const cardLeave = (event) => {
+	const cardMenu = event.target.querySelector('.card-menu')
+	cardMenu.style.display = 'none'
+	console.log('cardLeave....')
+}
+
+const removeCard = (event) => {
+	// parentElement sobe um nível na hierarquia de elementos enquanto closest busca um seletor nos ancestrais
+	// event.target.parentElement
+	event.target.closest('.card-ticker').remove()
+}
